8f677b1d359adcace277c485611d3459db5cdc46
kristiandz <36107078+kristiandz@users.noreply.github.com>
2025-02-19 16:25:51 +0100

    Setting up router and HttpClient

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { provideRouter } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { NotesComponent } from './notes/notes.component';
+import { NoteComponent } from './notes/note/note.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/notes', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: 'notes', component: NotesComponent },
+    { path: 'notes/:id', component: NoteComponent },
+  ];
+
+  export const appRouter = provideRouter(routes);
