6e9ff53b4d1236f4643338513b26f7b3850e0f18
kristiandz <36107078+kristiandz@users.noreply.github.com>
2025-02-21 09:16:38 +0100

    Exclude login/register from interceptor
    - No need to provide the token for auth endpoints, once the token is expired, this will error out as it wont be updated before the error itself

--- src/app/auth/auth-interceptor.module.ts
+++ src/app/auth/auth-interceptor.module.ts
@@ -6,8 +6,9 @@ import { catchError, throwError } from 'rxjs';
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const router = inject(Router);
   const authToken = localStorage.getItem('jwt');
+  const excludedPaths = ['/auth/login', '/auth/register', '/auth/refresh'];
 
-  if (authToken) {
+  if (!excludedPaths.some(path => req.url.includes(path)) && authToken) {
     req = req.clone({
       setHeaders: {
         Authorization: `Bearer ${authToken}`,
