e208d892cdd37c2a894b47ed76770b4c69789605
kristiandz <36107078+kristiandz@users.noreply.github.com>
2025-02-21 20:25:57 +0100

    ClickOutside Directive
    Implement the directive so clicks outside an element can be closed

---
+++ src/app/shared/click-outside.directive.ts
@@ -0,0 +1,16 @@
+import { Directive, ElementRef, HostListener, EventEmitter, Output } from '@angular/core';
+
+@Directive({ selector: '[clickOutside]'})
+export class ClickOutsideDirective {
+  @Output() clickOutside = new EventEmitter<void>();
+
+  constructor(private el: ElementRef) {}
+
+  @HostListener('document:click', ['$event'])
+  onClick(event: MouseEvent) {
+ 
+    if (!this.el.nativeElement.contains(event.target)) {
+      this.clickOutside.emit();
+    }
+  }
+}
