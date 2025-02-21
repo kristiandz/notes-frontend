c9a2f31f18df1eaf47bf967f2a88fa4975d301c9
kristiandz <36107078+kristiandz@users.noreply.github.com>
2025-02-21 20:27:30 +0100

    Update notes component
    - Added the click outside directory
    - Displaying all the fetched notes
    - In case one is selcted, displaying its full content instead of the table view

--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -1,19 +1,34 @@
+import { ClickOutsideDirective } from '../shared/click-outside.directive';
+import { NoteViewComponent } from './note-view/note-view.component';
+import { HeaderComponent } from '../header/header.component';
 import { Component, inject, Signal } from '@angular/core';
 import { NoteComponent } from './note/note.component';
-import { HeaderComponent } from '../header/header.component';
 import { NotesService } from './notes.service';
+import { Note } from './note/note.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-notes',
-  imports: [NoteComponent, HeaderComponent],
+  imports: [NoteComponent, HeaderComponent, NoteViewComponent, ClickOutsideDirective],
   templateUrl: './notes.component.html',
   styleUrl: './notes.component.scss',
 })
 export class NotesComponent {
   private notesService = inject(NotesService);
+  private router = inject(Router);
   notes: Signal<any[]> = this.notesService.notes;
+  selectedNote: Note | undefined = undefined;
 
   ngOnInit() {
     this.notesService.fetchNotes();
+  }
+
+  selectNote(id: number) {
+    this.selectedNote = this.notes()[id];
+    this.router.navigate(['/notes/' + id]);
+  }
+
+  closeSelectedNote() {
+    this.selectedNote = undefined;
   }
 }
--- src/app/notes/notes.service.ts
+++ src/app/notes/notes.service.ts
@@ -4,12 +4,19 @@ import { HttpClient } from '@angular/common/http';
 @Injectable({ providedIn: 'root' })
 export class NotesService {
   private http = inject(HttpClient);
-  private notesUrl = 'http://localhost:8080/notes/user/2';
+  private notesUrl = 'http://localhost:8080/';
   notes = signal<any[]>([]);
+  categories = signal<any[]>([]);
 
   fetchNotes() {
     this.http
-      .get<any[]>(this.notesUrl)
+      .get<any[]>(this.notesUrl  + 'notes/user/1')
       .subscribe((data) => this.notes.set(data));
+  }
+
+  fetchCategories() {
+    this.http
+      .get<any[]>(this.notesUrl + 'categories')
+      .subscribe((data) => this.categories.set(data));
   }
 }
