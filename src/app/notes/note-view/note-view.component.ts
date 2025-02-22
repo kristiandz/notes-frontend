55e6ac98c22f340325c140aef90e1a5255637c14
kristiandz <36107078+kristiandz@users.noreply.github.com>
2025-02-22 23:42:02 +0100

    Implemented delete for the selected note
    - Delete available for selected note
    - CSS adjustments

--- src/app/notes/note-view/note-view.component.ts
+++ src/app/notes/note-view/note-view.component.ts
@@ -1,6 +1,8 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { Note } from '../note/note.model';
 import { DatePipe } from '@angular/common';
+import { NotesService } from '../notes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-note-view',
@@ -10,5 +12,14 @@ import { DatePipe } from '@angular/common';
 })
 export class NoteViewComponent {
   note = input.required<Note>();
+  private router = inject(Router);
+  private notesService = inject(NotesService);
   
+  deleteNote(noteId: number){
+    this.notesService.deleteNote(noteId).subscribe(() => {
+      this.router.navigate(['../']);
+      this.notesService.updateFilteredNotes();
+    });
+  }
+
 }
