8c92c5e8ddf337dc0be9d26278c3c76a59c63e67
kristiandz <36107078+kristiandz@users.noreply.github.com>
2025-02-22 13:03:43 +0100

    Cleanup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { provideRouter } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { NotesComponent } from './notes/notes.component';
-import { NoteViewComponent } from './notes/note-view/note-view.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/notes', pathMatch: 'full' },
--- src/app/auth/auth-interceptor.module.ts
+++ src/app/auth/auth-interceptor.module.ts
@@ -10,7 +10,7 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authToken = localStorage.getItem('jwt');
   const excludedPaths = ['/auth/login', '/auth/register', '/auth/refresh'];
 
-  if (!excludedPaths.some(path => req.url.includes(path)) && authToken) {
+  if (!excludedPaths.some((path) => req.url.includes(path)) && authToken) {
     req = req.clone({
       setHeaders: {
         Authorization: `Bearer ${authToken}`,
