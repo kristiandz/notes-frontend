47dc09a2ae7b0691279f3793873cce905dd80f37
kristiandz <36107078+kristiandz@users.noreply.github.com>
2025-02-24 00:22:19 +0100

    Update sidebar
    - Correct routing for selected components

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Signal, createNgModuleRef, inject } from '@angular/core';
+import { NotesService } from '../notes/notes.service';
+import { AuthService } from '../auth/auth.service';
+import { Category, Note } from '../notes/note/note.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,5 +11,33 @@ import { Component } from '@angular/core';
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
-  isAuthorized=true;
+  private notesService = inject(NotesService);
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  categories: Signal<Category[]> = this.notesService.categories;
+  username = this.authService.getUsername();
+  isAuthorized = this.authService.getAuthorized();
+
+
+  changeCategory(categoryId: number | undefined) {
+    this.notesService.setCategory(categoryId);
+    if (categoryId !== undefined) {
+      this.router.navigate(['/notes/category', categoryId]);
+    } else {
+      this.router.navigate(['/notes']);
+    }
+  }
+
+  clearCategoryFilter(){
+    this.changeCategory(undefined);
+    this.router.navigate(['/notes']);
+  }
+
+  addNewNote(){
+    this.router.navigate(['/notes/new']);
+  }
+
+  manageCategories(){
+    this.router.navigate(['/categories']);
+  }
 }
