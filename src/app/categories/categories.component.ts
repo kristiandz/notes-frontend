072302e5a177f9accf257aed77a9512acf089870
kristiandz <36107078+kristiandz@users.noreply.github.com>
2025-02-24 04:32:36 +0100

    Basic category component
    - Render all components
    - Option to delete individual components

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from "../header/header.component";
+import { Component, inject, signal } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { CategoryService } from './category.service';
+import { Category } from '../notes/note/note.model';
 
 @Component({
   selector: 'app-categories',
   imports: [HeaderComponent],
   templateUrl: './categories.component.html',
-  styleUrl: './categories.component.scss'
+  styleUrl: './categories.component.scss',
 })
 export class CategoriesComponent {
+  categoryService = inject(CategoryService);
+  categories = signal<Category[]>([]);
 
+  ngOnInit() {
+    this.categoryService.getAllCategories().subscribe((data) => {
+      this.categories.set(data);
+    });
+  }
+
+  removeCategory(id: number) {
+    this.categoryService.removeCategory(id).subscribe(() => {
+      this.categories.set(
+        this.categories().filter((category) => category.id !== id)
+      );
+    });
+  }
 }
--- src/app/categories/category.service.ts
+++ src/app/categories/category.service.ts
@@ -1,13 +1,14 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { Category } from '../notes/note/note.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CategoryService {
-  private apiUrl = "http://localhost:8080";
+  private apiUrl = 'http://localhost:8080';
+  categories = signal<Category[]>([]);
 
   headers = new HttpHeaders({
     'Content-Type': 'application/json',
@@ -16,14 +17,24 @@ export class CategoryService {
   constructor(private http: HttpClient) {}
 
   createCategory(category: Category): Observable<Category> {
-    return this.http.post<Category>(this.apiUrl+"/categories", category, {
+    return this.http.post<Category>(this.apiUrl + '/categories', category, {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
       }),
     });
   }
 
+  getAllCategories(): Observable<Category[]> {
+    return this.http
+      .get<Category[]>(`${this.apiUrl}/categories`)
+      .pipe(tap((categories) => this.categories.set(categories)));
+  }
+
   removeCategory(categoryId: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/categories${categoryId}`);
+    return this.http.delete<void>(`${this.apiUrl}/categories/${categoryId}`);
+  }
+
+  fetchCategories(): void {
+    this.getAllCategories().subscribe();
   }
 }
